cdd832775ac9ca7bb909ecda92de1324994110df
Nimit Kanani <nimitmk7@gmail.com>
2024-07-30 21:22:22 -0400

    Changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-  output: 'export',
-  basePath: '/nimitkanani',
-  assetPrefix: '/nimitkanani/',
-  trailingSlash: true,
-  images: {
-    loader: 'imgix',
-    path: 'src/images'
-  }
-}
+module.exports = nextConfig
